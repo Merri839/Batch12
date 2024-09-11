5860b37c34bac1c516753208a8abe1ad1e03bc12
Merri839 <Merhhawitandit@gmail.com>
2024-09-10 18:53:34 -0700

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -5,7 +5,15 @@ public class GitHomework {
 public void display() {
 	System.out.println("Batch12");
 }
-	
+
+
+	/**
+            this is coming from GitHub
+	    java is fun
+            Git is easy
+	    API is not fun
+
+	**/
 	
 	
 	
