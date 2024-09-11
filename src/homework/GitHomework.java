eba56773bf6a004f4f8cef7db42957b320487915
Merri839 <Merhhawitandit@gmail.com>
2024-09-10 17:55:16 -0700

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -6,6 +6,8 @@ public class GitHomework {
 		System.out.println("BATCH 12: Git Class Lecture");
 	       int age = 12;
 		String name = "john";
+		System.out.println(age);
+		System.out.println(name);
 
 		//this is coming from github
 	}
@@ -13,10 +15,12 @@ public class GitHomework {
 	
 	public static void add(int a, int b) {
 		System.out.println(a+b);
+		//we are practising push and pull code
 	}
 	
 	public static void subtract(int a, int b) {
 		System.out.println(a-b);
+		
 	}
 	
 	public static void muliply(int a, int b) {
