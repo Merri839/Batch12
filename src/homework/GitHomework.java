89aae9c8d7c5eae1ece3e4389eaae435c5558ee5
merhh <merhh@192.168.0.32>
2024-09-10 18:15:22 -0700

    removed code

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,30 +2,7 @@ package homework;
 
 public class GitHomework {
 
-	public static void main (String args []) {
-		System.out.println("BATCH 12: Git Class Lecture");
-	       int age = 12;
-		String name = "john";
-		System.out.println(age);
-		System.out.println(name);
-
-		//this is coming from github
-	}
 	
-	
-	public static void add(int a, int b) {
-		System.out.println(a+b);
-		//we are practising push and pull code
-	}
-	
-	public static void subtract(int a, int b) {
-		System.out.println(a-b);
-		
-	}
-	
-	public static void muliply(int a, int b) {
-		System.out.println(a*b);
-	}
 	
 	
 	
