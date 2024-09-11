fef4f5c447481d5b59a064c0e77175703d634093
Merri839 <Merhhawitandit@gmail.com>
2024-09-10 17:50:19 -0700

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -3,7 +3,12 @@ package homework;
 public class GitHomework {
 
 	public static void main (String args []) {
-		System.out.println("BATCH 12: Git Class Lecture");	}
+		System.out.println("BATCH 12: Git Class Lecture");
+	       int age = 12;
+		String name = "john";
+
+		//this is coming from github
+	}
 	
 	
 	public static void add(int a, int b) {
