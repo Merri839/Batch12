8df9cb5ed6679f2fc57308d0c56fb2d44cd52694
merhh <merhh@192.168.0.32>
2024-09-10 19:06:24 -0700

    new code

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,20 +2,8 @@ package homework;
 
 public class GitHomework {
 
-public void display() {
-	System.out.println("Batch12");
-}
 
 
-	/**
-            this is coming from GitHub
-	    java is fun
-            Git is easy
-	    API is not fun
-
-	**/
-	
-	
 	
 	
 	
